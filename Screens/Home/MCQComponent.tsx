6e7686c459add42d859e9aee387980cd05a7f279
danish wadhwa <danish.wadhwa1997@gmail.com>
2023-12-09 00:25:03 +0530

    initial commit

---
+++ Screens/Home/MCQComponent.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const MCQComponent = () => {
+  return (
+    <View>
+      <Text>MCQComponent</Text>
+    </View>
+  );
+};
+
+export default MCQComponent;
+
+const styles = StyleSheet.create({});
